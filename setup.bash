962856d6ac3aaa95dd721492bb3ae4596cde7fd6
Tamir Madar <36384692+RollerSweet@users.noreply.github.com>
2023-12-05 00:49:15 +0200

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -3,17 +3,78 @@
 # Load environment variables from .env file
 source .env
 
+# Update /etc/sysctl.conf
+echo "net.ipv4.ip_unprivileged_port_start=80" | sudo tee -a /etc/sysctl.conf
+
+# Apply the changes
+sudo sysctl --system
+
 # Create XFS filesystem
 sudo mkfs.xfs /dev/sdb
 sudo mkdir /data
 UUID=$(sudo blkid -o value -s UUID /dev/sdb)
 echo "UUID=$UUID /data xfs defaults 0 2" | sudo tee -a /etc/fstab
 sudo mount /dev/sdb /data
-sudo mkdir -p /data/{influxdb,telegraf,grafana} && sudo chmod -R 777 /data
+sudo mkdir -p /data/{influxdb,telegraf,grafana,nginx/conf} && sudo chmod -R 777 /data
 sudo chown -R tmgvr:tmgvr /data
 
 # Configure firewall
-sudo firewall-cmd --permanent --add-port=8086/tcp --add-port=3000/tcp && sudo firewall-cmd --reload
+sudo firewall-cmd --permanent --add-port=8086/tcp --add-port=80/tcp --add-port=3000/tcp && sudo firewall-cmd --reload
+
+#Generate nginx configuration file
+cat <<EOF > /data/nginx/conf/nginx.conf
+events {
+    worker_connections  1024;
+}
+
+http {
+    # Basic settings for performance and efficiency
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_timeout 65;
+    types_hash_max_size 2048;
+
+    include /etc/nginx/mime.types;
+    default_type application/octet-stream;
+
+    # Logging settings
+    access_log /var/log/nginx/access.log;
+    error_log /var/log/nginx/error.log;
+
+    # Gzip Settings
+    gzip on;
+    gzip_disable "msie6";
+
+    # Configuration for Grafana
+    server {
+        listen 80;
+        server_name grafana.$DOMAIN_NAME;
+
+        location / {
+            proxy_pass http://$HOST_IP:3000;
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
+        }
+    }
+
+    # Configuration for InfluxDB
+    server {
+        listen 80;
+        server_name influxdb.$DOMAIN_NAME;
+
+        location / {
+            proxy_pass http://$HOST_IP:8086;
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
+        }
+    }
+}
+EOF
 
 # Generate telegraf configuration file
 cat <<EOF > /data/telegraf/telegraf.conf
@@ -119,9 +180,24 @@ cat <<EOF > /data/telegraf/telegraf.conf
   datacenter_metric_exclude = [ "*" ]
 EOF
 
+# Load the docker/podman images
+podman load < /home/tmgvr/script/images/nginx-latest.tar
+podman load < /home/tmgvr/script/images/telegraf-latest.tar
+podman load < /home/tmgvr/script/images/influxdb-2.0.tar
+podman load < /home/tmgvr/script/images/grafana-latest.tar
+
 # Create Docker volumes and network
 podman network create --driver bridge influxdb-telegraf-net
 podman volume create grafana-storage
+
+# Run nginx container
+podman run -d \
+    --name nginx-proxy \
+    -p 80:80 \
+    -v /data/nginx/conf/nginx.conf:/etc/nginx/nginx.conf:ro \
+    --privileged \
+    nginx:latest
+
 
 # Run InfluxDB container
 podman run -d --name=influxdb \
@@ -131,8 +207,24 @@ podman run -d --name=influxdb \
  --net=influxdb-telegraf-net \
  influxdb:2.0
 
-# Add a 10-second delay
-sleep 10
+# Run Grafana container
+podman run -d --name=grafana \
+  --restart=unless-stopped \
+  -p 3000:3000 \
+  -v grafana-storage:/var/lib/grafana \
+  -e "GF_SECURITY_ADMIN_USER=$GRAFANA_ADMIN_USER" \
+  -e "GF_SECURITY_ADMIN_PASSWORD=$GRAFANA_ADMIN_PASSWORD" \
+  grafana/grafana:latest
+  
+# Run Telegraf container
+podman run -d --name=telegraf \
+  --restart=unless-stopped \
+  -v /data/telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro \
+  --net=influxdb-telegraf-net \
+  --privileged \
+  telegraf:latest
+  
+sleep 30
 
 # Configure InfluxDB
 podman exec -it influxdb influx setup \
@@ -142,23 +234,6 @@ podman exec -it influxdb influx setup \
   --password "$INFLUXDB_PASSWORD" \
   --token "$INFLUXDB_TOKEN" \
   --force
-
-# Run Telegraf container
-podman run -d --name=telegraf \
-  --restart=unless-stopped \
-  -v /data/telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro \
-  --net=influxdb-telegraf-net \
-  --privileged \
-  telegraf
-
-# Run Grafana container
-podman run -d --name=grafana \
-  --restart=unless-stopped \
-  -p 3000:3000 \
-  -v grafana-storage:/var/lib/grafana \
-  -e "GF_SECURITY_ADMIN_USER=$GRAFANA_ADMIN_USER" \
-  -e "GF_SECURITY_ADMIN_PASSWORD=$GRAFANA_ADMIN_PASSWORD" \
-  grafana/grafana
 
 sleep 10
 
