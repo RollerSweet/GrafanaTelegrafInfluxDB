71882bdc504ce3be955b5f67273195a744756302
Tamir Madar <36384692+RollerSweet@users.noreply.github.com>
2023-12-06 00:43:26 +0200

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Load environment variables from .env file
-source .env
+CONFIG_FILE="/home/tmgvr/script/config.txt"
+source "$CONFIG_FILE"
 
 # Update /etc/sysctl.conf
 echo "net.ipv4.ip_unprivileged_port_start=80" | sudo tee -a /etc/sysctl.conf
@@ -9,19 +10,79 @@ echo "net.ipv4.ip_unprivileged_port_start=80" | sudo tee -a /etc/sysctl.conf
 # Apply the changes
 sudo sysctl --system
 
+# Containers will start after reboot
+systemctl --user enable podman-restart
+loginctl enable-linger $user
+
 # Create XFS filesystem
 sudo mkfs.xfs /dev/sdb
 sudo mkdir /data
 UUID=$(sudo blkid -o value -s UUID /dev/sdb)
 echo "UUID=$UUID /data xfs defaults 0 2" | sudo tee -a /etc/fstab
 sudo mount /dev/sdb /data
-sudo mkdir -p /data/{influxdb,telegraf,grafana,nginx/conf} && sudo chmod -R 777 /data
+sudo mkdir -p /data/{influxdb,telegraf,grafana/storage,nginx/conf} && sudo chmod -R 777 /data
 sudo chown -R tmgvr:tmgvr /data
+sudo chown -R 472:472 /data/grafana
+
 
 # Configure firewall
 sudo firewall-cmd --permanent --add-port=8086/tcp --add-port=80/tcp --add-port=3000/tcp && sudo firewall-cmd --reload
 
-#Generate nginx configuration file
+# Generate Grafana ldap configurations
+cat <<EOF > /data/grafana/ldap.toml
+[[servers]]
+# Active Directory server host
+host = "$DOMAIN_NAME"
+
+# Default port is 389 or 636 if using SSL
+port = 389
+
+# Use SSL & Skip SSL verification
+use_ssl = false
+ssl_skip_verify = true
+
+# Bind DN and password
+bind_dn = "CN=$GRAFANA_DOMAIN_USER,OU=Users,OU=ouP-Grafana,OU=ouP-0868,OU=ouProjects,$DOMAIN_NAME_SEP"
+bind_password = '$GRAFANA_DOMAIN_PASSWORD'
+
+# Search filter & base DN
+search_filter = "(sAMAccountName=%s)"
+search_base_dns = ["$DOMAIN_NAME_SEP"]
+
+# Map LDAP attributes to Grafana attributes
+[servers.attributes]
+name = "givenName"
+surname = "sn"
+username = "sAMAccountName"
+member_of = "memberOf"
+email = "mail"
+
+# Group mappings
+# Admin group mapping
+[[servers.group_mappings]]
+group_dn = "CN=sg-0868-GrafanaAdmins,OU=Security Groups,OU=ouP-Grafana,OU=ouP-0868,OU=ouProjects,$DOMAIN_NAME_SEP"
+org_role = "Admin"
+
+# Editor group mapping
+[[servers.group_mappings]]
+group_dn = "CN=sg-0868-GrafanaEditor,OU=Security Groups,OU=ouP-Grafana,OU=ouP-0868,OU=ouProjects,$DOMAIN_NAME_SEP"
+org_role = "Editor"
+
+# Viewer group mapping
+[[servers.group_mappings]]
+group_dn = "CN=sg-0868-GrafanaViewer,OU=Security Groups,OU=ouP-Grafana,OU=ouP-0868,OU=ouProjects,$DOMAIN_NAME_SEP"
+org_role = "Viewer"
+EOF
+
+# Generate grafana configuration file
+cat <<EOF > /data/grafana/grafana.ini
+[auth.ldap]
+enabled = true
+config_file = /etc/grafana/ldap.toml
+allow_sign_up = true
+EOF
+
+# Generate nginx configuration file
 cat <<EOF > /data/nginx/conf/nginx.conf
 events {
     worker_connections  1024;
@@ -188,43 +249,47 @@ podman load < /home/tmgvr/script/images/grafana-latest.tar
 
 # Create Docker volumes and network
 podman network create --driver bridge influxdb-telegraf-net
-podman volume create grafana-storage
 
 # Run nginx container
 podman run -d \
-    --name nginx-proxy \
-    -p 80:80 \
-    -v /data/nginx/conf/nginx.conf:/etc/nginx/nginx.conf:ro \
-    --privileged \
-    nginx:latest
-
+  --restart=always \
+  --name nginx-proxy \
+  -p 80:80 \
+  -v /data/nginx/conf/nginx.conf:/etc/nginx/nginx.conf:ro \
+  --privileged \
+  nginx:latest
 
 # Run InfluxDB container
 podman run -d --name=influxdb \
- --restart=unless-stopped \
- -p 8086:8086 \
- -v /data/influxdb:/root/.influxdb2 \
- --net=influxdb-telegraf-net \
- influxdb:2.0
+  --restart=always \
+  -p 8086:8086 \
+  -v /data/influxdb:/root/.influxdb2 \
+  --net=influxdb-telegraf-net \
+  influxdb:2.0
 
 # Run Grafana container
-podman run -d --name=grafana \
-  --restart=unless-stopped \
-  -p 3000:3000 \
-  -v grafana-storage:/var/lib/grafana \
-  -e "GF_SECURITY_ADMIN_USER=$GRAFANA_ADMIN_USER" \
-  -e "GF_SECURITY_ADMIN_PASSWORD=$GRAFANA_ADMIN_PASSWORD" \
+podman run -d -p 3000:3000 --name=grafana \
+  --restart=always \
+  --volume "/data/grafana/storage:/var/lib/grafana" \
+  --volume "/data/grafana/grafana.ini:/etc/grafana/grafana.ini" \
+  --volume "/data/grafana/ldap.toml:/etc/grafana/ldap.toml" \
+  -e "GF_AUTH_LDAP_ENABLED=true" \
+  -e "GF_AUTH_LDAP_CONFIG_FILE=/etc/grafana/ldap.toml" \
+  -e "GF_SECURITY_ADMIN_USER=${GRAFANA_ADMIN_USER}" \
+  -e "GF_SECURITY_ADMIN_PASSWORD=${GRAFANA_ADMIN_PASSWORD}" \
+  --privileged \
   grafana/grafana:latest
-  
+
+
 # Run Telegraf container
 podman run -d --name=telegraf \
-  --restart=unless-stopped \
+  --restart=always \
   -v /data/telegraf/telegraf.conf:/etc/telegraf/telegraf.conf:ro \
   --net=influxdb-telegraf-net \
   --privileged \
   telegraf:latest
-  
-sleep 30
+
+sleep 60
 
 # Configure InfluxDB
 podman exec -it influxdb influx setup \
@@ -235,7 +300,7 @@ podman exec -it influxdb influx setup \
   --token "$INFLUXDB_TOKEN" \
   --force
 
-sleep 10
+sleep 60
 
 curl -X POST "$GRAFANA_URL/api/datasources" \
     -H "Content-Type: application/json" \
@@ -263,3 +328,31 @@ curl -X POST "$GRAFANA_URL/api/datasources" \
           }
        }'
 
+sleep 5
+
+# Array of JSON dashboard paths
+DASHBOARD_JSON_PATHS=("/home/tmgvr/script/json/Overview.json" "/home/tmgvr/script/json/Datastore.json" "/home/tmgvr/script/json/VMs.json" "/home/tmgvr/script/json/Hosts.json")
+
+# Loop through each dashboard file and upload
+for DASHBOARD_JSON_PATH in "${DASHBOARD_JSON_PATHS[@]}"; do
+    if [[ -f "$DASHBOARD_JSON_PATH" ]]; then
+        # Read the JSON file
+        DASHBOARD_JSON=$(<"$DASHBOARD_JSON_PATH")
+
+        # Make the POST request to Grafana
+        RESPONSE=$(curl -s -k -X POST "$GRAFANA_URL/api/dashboards/db" \
+            -u "$GRAFANA_ADMIN_USER:$GRAFANA_ADMIN_PASSWORD" \
+            -H "Content-Type: application/json" \
+            -d "{\"dashboard\":$DASHBOARD_JSON,\"overwrite\":false}")
+
+        # Check response
+        if [[ $RESPONSE == *"\"status\":\"success\""* ]]; then
+            echo "Dashboard $DASHBOARD_JSON_PATH uploaded successfully."
+        else
+            echo "Failed to upload dashboard $DASHBOARD_JSON_PATH."
+            echo "Response: $RESPONSE"
+        fi
+    else
+        echo "Dashboard file $DASHBOARD_JSON_PATH not found."
+    fi
+done
